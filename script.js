6627445b6562d6b2f11431a69fdc89201b822899
vasile.moraru <chisinau2000@gmail.com>
2021-02-16 08:01:38 +0200

    ready app webSite_I

--- script.js
+++ script.js
@@ -1,17 +1,29 @@
-var myApp = angular.module("myList", []);
-myApp.controller("myListController", function ($scope) {
-    $scope.items = ['AngularJs', 'ReactJs', 'UndescoreJs'];
-    $scope.newItem = "";
+angular.module("todoApp", [])
+    .controller("TodoListController", function () {
+        var todoList = this;
+        todoList.todos = [
+            {text: 'learn AngularJS', done: true},
+            {text: 'build an AngularJS app', done: false}];
 
-    $scope.pushItem = function () {
-        if ($scope.newItem != "") {
-            $scope.items.push($scope.newItem);
-            $scope.newItem = "";
-        }
-    }
+        todoList.remaining = function () {
+            var count = 0;
+            angular.forEach(todoList.todos, function (todo) {
+                count += todo.done ? 0 : 1;
+            });
+            return count;
+        };
 
-    $scope.deleteItem = function (index) {
-        $scope.items.splice(index, 1);
-    }
+        todoList.archive = function () {
+            var oldTodos = todoList.todos;
+            todoList.todos = [];
+            angular.forEach(oldTodos, function (todo) {
+                if (!todo.done) todoList.todos.push(todo);
+            });
+        };
 
-})
+        todoList.addTodo = function() {
+            todoList.todos.push({text:todoList.todoText, done:false});
+            todoList.todoText = '';
+        };
+    });
+
