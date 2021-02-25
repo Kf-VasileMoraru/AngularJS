208c03d630b40e5b9012c889500ae0a1dcfd28b0
vasile.moraru <chisinau2000@gmail.com>
2021-02-25 17:07:44 +0200

    async

--- testNpm.js
+++ testNpm.js
@@ -1,3 +1,27 @@
-// npm init -y
-// node testNpm.js
-console.log('vasea');
+function myDisplayer(some) {
+    console.log(some)
+}
+
+function myCalculator(num1, num2, myCallback) {
+    let sum = num1 + num2;
+    myCallback(sum);
+    return "ion";
+}
+
+
+new Promise((x)=>{
+        setTimeout(()=>{
+            x(()=>myCalculator(5, 5, myDisplayer))
+        }, 2000);})
+    .then(x=>{
+    console.log(x())});
+
+
+
+ function test(){
+return  setTimeout(()=>{
+        x(myCalculator(5, 5, myDisplayer))
+    }, 3000);
+
+}
+test()
